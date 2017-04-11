6fa97950cdb1b6cf6ee2ebecc810019eaf34ee78
MichelML <michmoreau.l@gmail.com>
2017-04-11 00:22:16 -0400

    remove error in indexjs

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ const terminalOptions = require('./lib/terminal-options')
 const configFile = require('./lib/config-file')
 const shell = require('shelljs')
 
-try {
 if (terminalOptions.hasConfigFlag() && terminalOptions.isResetConfigRequested()) {
     configFile.resetConfig()
 } else if (terminalOptions.hasConfigFlag()) {
     configFile.openConfig()
 } else {
     require('./pull-request')()
-} } catch (e) {
-    
-}
+} 
