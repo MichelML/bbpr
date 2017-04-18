c301cf325e9b8331cb68d3351be8fd273fa175ad
MichelML <michmoreau.l@gmail.com>
2017-04-17 21:12:00 -0400

    allow remote config file set up

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ if (terminalOptions.hasConfigFlag() && terminalOptions.isResetConfigRequested())
   configFile.resetConfig()
 } else if (terminalOptions.hasConfigFlag() && terminalOptions.isAllTrueConfigRequested()) {
   configFile.setAllTrueConfig()
+} else if (terminalOptions.hasConfigFlag() && terminalOptions.isTailoredConfigRequested() && terminalOptions.isRemoteConfigRequested()) {
+  configFile.setRemoteConfig(terminalOptions.getTailoredConfigFilePath())
 } else if (terminalOptions.hasConfigFlag() && terminalOptions.isTailoredConfigRequested()) {
   configFile.setTailoredConfig(terminalOptions.getTailoredConfigFilePath())
 } else if (terminalOptions.hasConfigFlag()) {
