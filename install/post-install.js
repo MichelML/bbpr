4f766b55831ed70665a59f308ceaa75bf476388b
MichelML <michmoreau.l@gmail.com>
2017-04-25 01:05:48 -0400

    test install scripts

--- install/post-install.js
+++ install/post-install.js
@@ -1,11 +1,11 @@
-#!/ usr/bin/env node
-const fs = require('fs')
+// #!/ usr/bin/env node
+// const fs = require('fs')
 
-fs.stat('../bbpr.config.backup.js', rewriteConfigBackup)
+// fs.stat('../bbpr.config.backup.js', rewriteConfigBackup)
 
-function rewriteConfigBackup (error) {
-  if (!error) {
-    fs.writeFileSync('bbpr.config.js', fs.readFileSync('../bbpr.config.backup.js', 'utf-8'))
-    fs.unlinkSync('../bbpr.config.backup.js')
-  }
-}
+// function rewriteConfigBackup (error) {
+//   if (!error) {
+//     fs.writeFileSync('bbpr.config.js', fs.readFileSync('../bbpr.config.backup.js', 'utf-8'))
+//     fs.unlinkSync('../bbpr.config.backup.js')
+//   }
+// }
