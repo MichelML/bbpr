0db35b3ad8d50d117a21221cc1b650af5448aecf
MichelML <michmoreau.l@gmail.com>
2017-04-26 20:33:57 -0400

    use path module install of path strings
    fix npm install scripts

--- post-install.js
+++ post-install.js
@@ -1,8 +1,9 @@
 #!/ usr/bin/env node
 const fs = require('fs')
 const path = require('path')
-const srcPath = path.join(__dirname, '..', '/bbpr.backup.config.js')
-const destPath = path.join(__dirname, '/bbpr.config.js')
+const pathToNodeModules = path.resolve().substr(0, path.resolve().lastIndexOf('/'))
+const srcPath = path.join(pathToNodeModules, 'bbpr.backup.config.js')
+const destPath = './bbpr.config.js'
 
 if (fs.existsSync(srcPath)) {
   fs.writeFileSync(destPath, fs.readFileSync(srcPath, 'utf8'))
--- pre-install.js
+++ pre-install.js
@@ -1,8 +1,9 @@
 #!/ usr/bin/env node
 const fs = require('fs')
 const path = require('path')
-const srcPath = path.join(__dirname, '/bbpr.config.js')
-const destPath = path.join('../../', 'bbpr.backup.config.js')
+const pathToNodeModules = [1, 2].reduce(pathToNode => pathToNode.substr(0, pathToNode.lastIndexOf('/')), path.resolve())
+const srcPath = path.join(pathToNodeModules, 'bbpr/bbpr.config.js')
+const destPath = path.join(pathToNodeModules, 'bbpr.backup.config.js')
 
 if (fs.existsSync(srcPath)) {
   fs.writeFileSync(destPath, fs.readFileSync(srcPath, 'utf8'))
