b49b1af03c5681188b377bd9b80a16e29432fe2f
MichelML <michmoreau.l@gmail.com>
2017-05-07 01:17:05 -0400

    add tests for strings lib

--- spec/lib/strings.spec.js
+++ spec/lib/strings.spec.js
@@ -1,1 +1,20 @@
 // const strings = require('../../lib/strings')
+describe('strings.js', () => {
+  it('should require without error', () => {
+    expect(() => require('../../lib/strings')).not.toThrow()
+  })
+
+  it('should have only strings as end values', () => {
+    function checkIfEndValueIsString (obj) {
+      for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+          checkIfEndValueIsString(obj[key])
+        } else {
+          expect(typeof obj[key]).toBe('string')
+        }
+      }
+    }
+
+    checkIfEndValueIsString(require('../../lib/strings'))
+  })
+})
