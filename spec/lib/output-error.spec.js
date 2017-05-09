01c67927438226b25d8b4d09a48f67ee76ee812a
MichelML <michmoreau.l@gmail.com>
2017-05-09 19:02:36 -0400

    add require test for output error lib

--- spec/lib/output-error.spec.js
+++ spec/lib/output-error.spec.js
@@ -1,38 +1,45 @@
-const outputError = require('../../lib/output-error')
 
 describe('output-error.js', () => {
   let processExit
   let consoleLog
 
-  beforeEach(() => {
-    processExit = spyOn(process, 'exit')
-    consoleLog = spyOn(console, 'log')
+  it('should require without error', () => {
+    expect(() => require('../../lib/output-error')).not.toThrow()
   })
 
-  it('should not throw if argument pass to outputError is a string', () => {
-    expect(() => outputError('this is an error string')).not.toThrow()
-  })
+  describe('after require', () => {
+    const outputError = require('../../lib/output-error')
 
-  it('should not throw if argument pass to outputError is an object with a message property', () => {
-    expect(() => outputError({ message: 'this is an error message'})).not.toThrow()
-  })
+    beforeEach(() => {
+      processExit = spyOn(process, 'exit')
+      consoleLog = spyOn(console, 'log')
+    })
 
-  it('should not throw if is not an object with message or a string', () => {
-    expect(() => outputError()).not.toThrow()
-    expect(() => outputError({})).not.toThrow()
-    expect(() => outputError([])).not.toThrow()
-  })
+    it('should not throw if argument pass to outputError is a string', () => {
+      expect(() => outputError('this is an error string')).not.toThrow()
+    })
 
-  it('should console log a message and an intro to the message', () => {
-    outputError('this is an error string')
-    expect(consoleLog).toHaveBeenCalledTimes(2)
-  })
+    it('should not throw if argument pass to outputError is an object with a message property', () => {
+      expect(() => outputError({ message: 'this is an error message'})).not.toThrow()
+    })
 
-  it('should exit the program', () => {
-    outputError('any message')
-    expect(processExit).toHaveBeenCalledTimes(1)
+    it('should not throw if is not an object with message or a string', () => {
+      expect(() => outputError()).not.toThrow()
+      expect(() => outputError({})).not.toThrow()
+      expect(() => outputError([])).not.toThrow()
+    })
 
-    outputError({ message: 'any message' })
-    expect(processExit).toHaveBeenCalledTimes(2)
+    it('should console log a message and an intro to the message', () => {
+      outputError('this is an error string')
+      expect(consoleLog).toHaveBeenCalledTimes(2)
+    })
+
+    it('should exit the program', () => {
+      outputError('any message')
+      expect(processExit).toHaveBeenCalledTimes(1)
+
+      outputError({ message: 'any message' })
+      expect(processExit).toHaveBeenCalledTimes(2)
+    })
   })
 })
