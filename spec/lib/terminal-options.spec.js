06b229618d717985ae47e6cd47d6cf2c92165526
MichelML <michmoreau.l@gmail.com>
2017-05-13 14:59:51 -0400

    test one function

--- spec/lib/terminal-options.spec.js
+++ spec/lib/terminal-options.spec.js
@@ -2,4 +2,15 @@ describe('terminal-options.js', () => {
   it('should require without error', () => {
     expect(() => require('../../lib/terminal-options')).not.toThrow()
   })
+
+  describe('after require', () => {
+    const terminalOptions = require('../../lib/terminal-options')
+    const filePath = 'filePath'
+
+    describe('getProccessArgV', () => {
+      it('should return the terminal arguments', () => {
+        expect(terminalOptions.getProcessArgV()).toEqual(process.argv)
+      })
+    })
+  })
 })
